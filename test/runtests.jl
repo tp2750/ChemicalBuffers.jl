4e57c690b3c5fcacd46474daff6f0a6995e72b8e
Thomas Poulsen <ta.poulsen@gmail.com>
2022-12-11 15:51:11 +0100

    add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
 using ChemicalBuffers
 using Test
 
+sd(x) = sum(vec(hcat(x...)).^2)
+
 @testset "ChemicalBuffers.jl" begin
-    # Write your tests here.
+    @test ionization_fractions_gamma1(acetic_acid, 4.76) == [.5, .5]
+    ca2 = ionization_fractions_gamma1.(Ref(carbonic_acid), [6.35, 10.33])
+    @test sd(ca2 - [[.5, .5, 0], [0, .5, .5]]) < 1E-5
 end
